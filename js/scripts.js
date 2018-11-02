8a5219fd3783494839f6f5d7cbe3fbb9571245c7
John Michael Brown <jmichaelbrown132737@gmail.com>
2018-11-02 12:03:51 -0700

    alter Pizza prototypes to be able to handle a wider range of inputs

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,37 @@
  // Business Logic -------------------------------------------------------------
 
-function Pizza (name, topping, size = 1){
+function Pizza (name, size = 1, topping = "Cheese") {
   this.name = name,
   this.size = size,
   this.topping = topping
 }
 
-Pizza.prototype.changeSize = function(newSize){
-  this.size = newSize;
+Pizza.prototype.changeSize = function(newSize) {
+  if (newSize && newSize < 4 && newSize > 0) {
+    this.size = newSize;
+  }
   return this.size;
 }
 Pizza.prototype.changeTopping = function(toppings, newToppingIndex){
-  this.topping = toppings[newToppingIndex];
+  if (toppings && newToppingIndex) {
+    this.topping = toppings[newToppingIndex];
+  }
   return this.topping;
 }
-Pizza.prototype.changePrice = function(newPrice){
-  this.price = newPrice;
+Pizza.prototype.changePrice = function(newPrice) {
+  var price = newPrice;
+  if (parseInt(newPrice)) {
+    price = (Math.floor(Math.round(price*100)))/100;
+    this.price = price;
+  }
   return this.price;
 }
-
-var myPizza = new Pizza ("my pizza", 2, "Pepperoni");
-var toppings = ["Cheese", "Pepperoni", "Meat Lovers", "Supreme"];
-
 
 Pizza.prototype.findPriceOfPizza = function(toppings) {
   var currentToppings = toppings;
   var size = this.size;
   var pizzaTopping = this.topping;
-  var newPrice = 10;
+  var newPrice = 9.99;
 
   if (size === 2) {
     newPrice += 3;
@@ -43,5 +47,13 @@ Pizza.prototype.findPriceOfPizza = function(toppings) {
   this.changePrice(newPrice);
   return newPrice;
 }
+
+function Topping (toppingName, toppingCost) {
+  this.cost = toppingCost,
+  this.toppingName = toppingName
+} // not currently being used
+
+var myPizza = new Pizza ("my pizza");
+var toppings = ["Cheese", "Pepperoni", "Meat Lovers", "Supreme"];
 
 // Considering making topping an object
